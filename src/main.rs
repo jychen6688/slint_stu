3be8f89d33d86c690748762cb54effffca869aa4
jychen <jychen@studio.local>
2025-07-20 22:34:46 +0800

    实现无边框模式下的窗口拖拽功能

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use slint::PhysicalPosition;
+
 slint::include_modules!();
 fn main() {
     let app = APP::new().unwrap();
+    let app_drage_window_weak = app.as_weak();
+    app.on_drag_window(move |delta_x, delta_y| {
+        let app = app_drage_window_weak.unwrap();
+        let window = app.window();
+        let scale = window.scale_factor();
+
+        let position = window.position();
+        let x = position.x + (delta_x * scale) as i32;
+        let y = position.y + (delta_y * scale) as i32;
+
+        window.set_position(PhysicalPosition::new(x, y));
+    });
+
     app.run().unwrap();
 }
